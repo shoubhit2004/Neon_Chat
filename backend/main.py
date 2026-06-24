c9854133daddd94260ec7d19b7a4136ae003363e
Shoubhit Banerjee <banerjeeshoubhit@gmail.com>
2026-06-25 01:16:28 +0530

    Backend Update CORS

--- backend/main.py
+++ backend/main.py
@@ -31,7 +31,7 @@ app.add_middleware(SlowAPIMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
